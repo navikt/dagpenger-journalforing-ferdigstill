e9b8800e5007fe6ff76c48f2f1ab13810fe7c3bb
bendreas <andreas.nygard.bergh@nav.no>
2020-03-02 11:42:57 +0100

    Legg til manglende filer som gjorde at man ikke kunne kompilere

---
+++ src/main/kotlin/no/nav/dagpenger/journalføring/ferdigstill/adapter/vilkårtester/BehovStatusPoller.kt
@@ -0,0 +1,83 @@
+package no.nav.dagpenger.journalføring.ferdigstill.adapter.vilkårtester
+
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.moshi.responseObject
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.time.delay
+import kotlinx.coroutines.withTimeout
+import mu.KotlinLogging
+import java.time.Duration
+
+private val LOGGER = KotlinLogging.logger {}
+
+class BehovStatusPoller(
+    private val regelApiUrl: String,
+    private val regelApiKey: String,
+    private val timeout: Duration = Duration.ofSeconds(20)
+) {
+    private val delayDuration = Duration.ofMillis(100)
+
+    private fun pollInternal(statusUrl: String): BehovStatusPollResult {
+        val (_, response, result) =
+            with(
+                statusUrl
+                    .httpGet()
+                    .apiKey(regelApiKey)
+                    .allowRedirects(false)
+            ) { responseObject<BehovStatusResponse>() }
+
+        return try {
+            BehovStatusPollResult(result.get().status, null)
+        } catch (exception: Exception) {
+            if (response.statusCode == 303) {
+                LOGGER.info("Caught 303: $response")
+                return BehovStatusPollResult(
+                    null,
+                    response.headers["Location"].first()
+                )
+            } else {
+                throw PollSubsumsjonStatusException(
+                    response.responseMessage, exception
+                )
+            }
+        }
+    }
+
+    suspend fun pollStatus(statusUrl: String): String {
+        val url = "$regelApiUrl$statusUrl"
+        try {
+            return withTimeout(timeout.toMillis()) {
+                return@withTimeout pollWithDelay(url)
+            }
+        } catch (e: Exception) {
+            when (e) {
+                is TimeoutCancellationException -> throw RegelApiTimeoutException("Polled behov status for more than ${timeout.toMillis()} milliseconds")
+                else -> throw PollSubsumsjonStatusException("Failed", e)
+            }
+        }
+    }
+
+    private suspend fun pollWithDelay(statusUrl: String): String {
+        val status = pollInternal(statusUrl)
+        return if (status.isPending()) {
+            delay(delayDuration)
+            pollWithDelay(statusUrl)
+        } else {
+            status.location ?: throw PollSubsumsjonStatusException("Did not get location with task")
+        }
+    }
+}
+
+private data class BehovStatusPollResult(
+    val status: BehovStatus?,
+    val location: String?
+) {
+    fun isPending() = status == BehovStatus.PENDING
+}
+
+class PollSubsumsjonStatusException(
+    override val message: String,
+    override val cause: Throwable? = null
+) : RuntimeException(message, cause)
+
+class RegelApiTimeoutException(override val message: String) : RuntimeException(message)
