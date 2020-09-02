050265c28fe0d3d009fff39bf76555fd5718534c
Marte Pedersen <pedersen.marte@gmail.com>
2020-09-02 14:55:10 +0200

    lagt til test-data for oppslag, og satt opp litt mer i medlemskaptesten

--- src/test/kotlin/no/nav/dagpenger/journalføring/ferdigstill/HentMedlemskapChainTest.kt
+++ src/test/kotlin/no/nav/dagpenger/journalføring/ferdigstill/HentMedlemskapChainTest.kt
@@ -1,9 +1,11 @@
 package no.nav.dagpenger.journalføring.ferdigstill
 
 import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
 import io.mockk.mockk
 import no.nav.dagpenger.events.Packet
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
 
 class HentMedlemskapChainTest {
 
@@ -12,11 +14,13 @@ class HentMedlemskapChainTest {
 
         val medlemskapOppslagClient: MedlemskapOppslagClient = mockk()
         val hentMedlemskapChain = HentMedlemskapChain(medlemskapOppslagClient = medlemskapOppslagClient, neste = null)
-
+        val fnr = "12345678919"
         val packet = Packet().apply {
-            this.putValue(PacketKeys.NATURLIG_IDENT, "12345678919")
+            this.putValue(PacketKeys.NATURLIG_IDENT, fnr)
         }
 
+        coEvery { medlemskapOppslagClient.hentMedlemskapsevaluering(fnr, any(), any(), any(), any()) } returns
+                Medlemskapsevaluering(LocalDateTime.now(), "", "", emptyMap(), Medlemskapresultat("", "", "", "JA", emptyList()))
         hentMedlemskapChain.håndter(packet)
         packet.hasField(PacketKeys.MEDLEMSKAP) shouldBe true
         packet.getBoolean(PacketKeys.MEDLEMSKAP) shouldBe true
