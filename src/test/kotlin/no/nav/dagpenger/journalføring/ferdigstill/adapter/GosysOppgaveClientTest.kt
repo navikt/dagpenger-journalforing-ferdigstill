78185daffef06dd97356b58b8d0a1328e831e41c
Geir André Lund <geir.andre.lund@nav.no>
2020-09-03 09:50:14 +0200

    Ignore import violation ktlint!

--- src/test/kotlin/no/nav/dagpenger/journalføring/ferdigstill/adapter/GosysOppgaveClientTest.kt
+++ src/test/kotlin/no/nav/dagpenger/journalføring/ferdigstill/adapter/GosysOppgaveClientTest.kt
@@ -2,6 +2,7 @@ package no.nav.dagpenger.journalføring.ferdigstill.adapter
 
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo // ktlint-disable
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration
 import com.github.tomakehurst.wiremock.matching.EqualToPattern
 import com.gregwoodfill.assert.shouldStrictlyEqualJson
