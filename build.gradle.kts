77d7c90dd6130047fa0178c38aab5547baef4a9a
Geir André Lund <geir.andre.lund@nav.no>
2020-09-09 09:35:17 +0200

    Bruk kotlin coroutines v 1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -130,7 +130,7 @@ dependencies {
     testImplementation(KafkaEmbedded.env)
     testImplementation(Kafka.streamTestUtils)
     testImplementation(Wiremock.standalone)
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.9")
+    testImplementation(Kotlin.Coroutines.module("test"))
 
     testRuntimeOnly(Junit5.engine)
 
