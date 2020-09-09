673cd06da00be27c19413a44624c08a1d8fd0e19
Geir André Lund <geir.andre.lund@nav.no>
2020-09-09 11:28:34 +0200

    log4j2 og logback kræsj

--- build.gradle.kts
+++ build.gradle.kts
@@ -105,12 +105,12 @@ dependencies {
     implementation(Konfig.konfig)
     implementation(Ktor.serverNetty)
 
-    // logging
+    // Logging
     implementation(Kotlin.Logging.kotlinLogging)
-    implementation(Log4j2.api)
-    implementation(Log4j2.core)
-    implementation(Log4j2.slf4j)
-    implementation(Log4j2.Logstash.logstashLayout)
+    // logback (brought in by rapid-rivers)
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+
+    // ulids
     implementation(Ulid.ulid)
 
     // resilience
