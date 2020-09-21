0ae0f8f153de40ed21882dc9da653476c09d9da7
Geir André Lund <geir.andre.lund@nav.no>
2020-09-21 15:35:04 +0200

    Skip journalpost med id 485283071

--- src/main/kotlin/no/nav/dagpenger/journalføring/ferdigstill/JournalføringFerdigstill.kt
+++ src/main/kotlin/no/nav/dagpenger/journalføring/ferdigstill/JournalføringFerdigstill.kt
@@ -21,7 +21,7 @@ import java.time.ZoneId
 
 private val logger = KotlinLogging.logger {}
 
-private val ignorerJournalpost: Set<String> = setOf("477201031", "476557172", "475680871", "471479059", "471479060", "471478910", "467047358")
+private val ignorerJournalpost: Set<String> = setOf("477201031", "476557172", "475680871", "471479059", "471479060", "471478910", "467047358", "485283071")
 
 internal val erIkkeFerdigBehandletJournalpost = Predicate<String, Packet> { _, packet ->
     packet.hasField(PacketKeys.JOURNALPOST_ID) &&
